7f1db22d0ee9ee77c74937c4e4ded41e6b12ff5f
Javier <andrianijavieranalistaqa@gmail.com>
2022-08-08 00:18:36 -0300

    commit de prueba

--- cypress/integration/seccion_1/Opciones_click.js
+++ cypress/integration/seccion_1/Opciones_click.js
@@ -18,7 +18,9 @@ describe("Opciones de click", () => {
         cy. get("#menu_admin_viewAdminModule > b").should("be.visible").click()
         cy.wait(1500)
         cy. get("#menu_time_viewTimeModule > b").should("be.visible").click()
-    
+        
+
+        //probando 
     })
 
 })// Cierre de Describe
